6c13382dab10cc6a1c912f0b899643ffd3bf78bb
ASTRALLIBERTAD <astrallibertad@gmail.com>
2026-03-31 20:26:46 +0800

    extension video

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from typing import cast
+from PIL import Image, ImageSequence
+from manim.utils.color import YELLOW_A
+import numpy as np
 from manim import (
     BOLD,
     PURPLE,
@@ -33,6 +36,7 @@ from manim import (
     Restore,
     ApplyMethod,
 )
+from svgelements import Length
 
 
 class IntroScene(Scene):
@@ -49,13 +53,15 @@ class IntroScene(Scene):
             color=GREY_A, fill_color=GREY_A, fill_opacity=1.0, height=0.5, width=0
         ).move_to(full_text[0])
 
-        self.play(TypeWithCursor(full_text, cursor), runtime=0.5)
+        self.play(
+            TypeWithCursor(full_text, cursor, run_time=0.8),
+        )
 
         self.play(Blink(cursor, blinks=2))
 
         exec_text = Text("Executing...", font=font_term, font_size=20, color=GREEN)
         exec_text.next_to(prompt, DOWN, aligned_edge=LEFT)
-        self.play(Write(exec_text))
+        self.play(Write(exec_text, run_time=0.8))
         self.wait(1)
 
         # 8. Fade out everything
@@ -200,12 +206,17 @@ class IntroScene(Scene):
             os_part.animate.scale(1 / 1.5).set_opacity(0.3),
         )
 
-        self.play(
-            FadeOut(lainst_sub, rust_img, rust_txt, godot_img, linux_img, Windows_img)
-        )
-
         about_me = Text("About Me", gradient=(RED, BLUE)).scale(1.3)
         about_me.to_edge(UP)
+
+        self.play(
+            FadeOut(lainst_sub, rust_img, rust_txt, godot_img, linux_img, Windows_img),
+            Transform(interest, about_me),
+            FadeOut(interest),
+        )
+
+        self.play(about_me.animate.scale(1.4), run_time=0.4)
+        self.play(about_me.animate.scale(1 / 1.4), run_time=0.4)
 
         hobby = Text(
             "In my spare time, I enjoy building my own projects, playing Minecraft,",
@@ -219,41 +230,154 @@ class IntroScene(Scene):
         mine_part = hobby[49:58]
 
         proj_part.save_state()
+
+        proj = ImageMobject("projects.PNG").set_z_index(5)
+
         mine_part.save_state()
+
+        world = ImageMobject("world.jpeg").set_opacity(0)
 
         cursor.next_to(hobby[0])
 
         self.play(
             Succession(
                 TypeWithCursor(cast("Text", hobby[:41]), cursor, run_time=3),
+                FadeIn(proj),
                 ApplyMethod(proj_part.scale, 1.5, run_time=0.8),
                 AnimationGroup(
                     TypeWithCursor(cast("Text", hobby[41:60]), cursor, run_time=2),
                     Restore(proj_part, run_time=0.8),
                 ),
-                ApplyMethod(mine_part.scale, 1.5, run_time=0.8),
-                Restore(mine_part, run_time=0.8),
             )
         )
 
+        self.play(
+            FadeOut(proj),
+            FadeIn(world.set_opacity(1)),
+            mine_part.animate.scale(1.5),
+            run_time=0.8,
+        )
+
+        self.play(Restore(mine_part), FadeOut(world), run_time=1)
+
         self.play(FadeOut(hobby, cursor, run_time=0.3))
+
+        scratch = ImageMobject("scratch.png").scale(1.2)
 
         line1_int = Text(
             "and one interesting fact about me is that I'm currently",
             font_size=28,
         ).to_edge(DOWN, buff=1.2)
 
+        programming_brain = (
+            ImageMobject("programming_brain.PNG").scale(1.1).set_z_index(-1)
+        )
+
         line2_int = Text(
-            "making my own compiler from scratch,",
-            t2c=cast(dict[str, str], {"compiler": BLUE, "scratch": RED}),
+            "making my own programming language from scratch,",
+            t2c=cast(dict[str, str], {"programming language": BLUE, "scratch": RED}),
             font_size=28,
         ).next_to(line1_int, DOWN)
 
         cursor.next_to(line1_int[0], LEFT, buff=0.05)
 
-        self.play(TypeWithCursor(line1_int, cursor))
+        self.play(
+            TypeWithCursor(line1_int, cursor, run_time=3.2),
+            FadeIn(programming_brain, run_time=0.8),
+        )
 
         cursor.next_to(line2_int[0])
-        self.play(TypeWithCursor(line2_int, cursor))
+        self.play(
+            TypeWithCursor(line2_int, cursor),
+            FadeOut(programming_brain),
+            FadeIn(scratch),
+        )
 
-        self.play(FadeOut(line1_int, line2_int, cursor))
+        self.play(FadeOut(line1_int, line2_int, cursor, scratch))
+
+        brain_logo = ImageMobject("mascot.png").scale(1.5)
+
+        prog_name = Text(
+            "which I named Brain.",
+            t2c=cast(dict[str, str], {"Brain": RED}),
+            font_size=28,
+        ).to_edge(DOWN)
+
+        brain_txt = Text(
+            "BRAIN", t2c=cast(dict[str, str], {"BRAIN": YELLOW_A}), font_size=37
+        ).next_to(brain_logo, DOWN)
+
+        example_brain = ImageMobject("example_brain.PNG")
+
+        self.play(
+            FadeIn(prog_name, brain_logo),
+        )
+
+        self.wait(1.8)
+
+        self.play(
+            brain_logo.animate.to_edge(LEFT), FadeIn(example_brain), FadeIn(brain_txt)
+        )
+
+        self.wait(1.8)
+
+        expectation = Text("Expectations", gradient=(BLUE, RED)).scale(1.3).to_edge(UP)
+
+        expectations_line1 = Text(
+            "From this course, I expect to enhance my communication skills and",
+            t2c=cast(dict[str, str], {"enhance": RED}),
+            font_size=28,
+        ).to_edge(DOWN, buff=1.2)
+
+        cursor.next_to(expectations_line1)
+
+        self.play(
+            Transform(about_me, expectation),
+            FadeOut(brain_logo, prog_name, brain_txt, example_brain, run_time=0.8),
+            TypeWithCursor(expectations_line1, cursor, run_time=2.5),
+        )
+
+        expectations_line2 = Text(
+            "learn how to express my ideas more clearly and effectively.",
+            t2c=cast(
+                dict[str, str],
+                {
+                    "express": PURPLE,
+                    "ideas": YELLOW,
+                    "clearly": BLUE,
+                    "effectively": GREEN,
+                },
+            ),
+            font_size=28,
+        ).next_to(expectations_line1, DOWN)
+
+        cursor.next_to(expectations_line2)
+
+        monkey = ImageMobject("monkey.png").scale(0.4)
+
+        self.play(
+            TypeWithCursor(expectations_line2, cursor, run_time=2.5), FadeIn(monkey)
+        )
+
+        self.wait(3)
+
+        self.play(FadeOut(monkey), FadeOut(expectations_line1, expectations_line2))
+
+        gif_path = "assets/astra.gif"
+        pil_gif = Image.open(gif_path)
+
+        frames = [
+            np.array(frame.convert("RGBA")) for frame in ImageSequence.Iterator(pil_gif)
+        ]
+
+        gif_mobject = ImageMobject(frames[0])
+        gif_mobject.scale(1.5)
+
+        def update_gif(mob, dt):
+            frame_idx = int(self.time * 20) % len(frames)
+            mob.pixel_array = frames[frame_idx]
+
+        gif_mobject.add_updater(update_gif)
+
+        self.add(gif_mobject)
+        self.wait(5)
